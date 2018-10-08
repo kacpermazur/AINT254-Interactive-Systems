cf557e1f7b7b44e4f50be1755bb82930879df376
Kacper Mazur <33032053+kacpermazur@users.noreply.github.com>
2018-10-08 20:50:44 +0000

    HelloWorld
    Basic Implmentation Of:
    - Movement
    - Gravity Change
    - Attract

---
+++ GravityProject/Assets/Scripts/Physics/AttractTo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttractTo : MonoBehaviour
+{
+
+	[SerializeField]
+	private GameObject _playerPos;
+	
+	private Rigidbody _object;
+
+	private Vector3 _direction;
+	private float _distance;
+	private float _force;
+	
+	// mass1 * mass2 / distance
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_object = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		AttractToObject();
+	}
+
+	private void AttractToObject()
+	{
+		// Distance from BlackHole To Player
+		_direction = _object.position - _playerPos.transform.position;
+		_distance = _direction.magnitude;
+		
+		// Mass Of Two Obj To Get Force
+		_force = (_object.mass * _playerPos.GetComponent<Rigidbody>().mass);
+		
+		//Same Direction Towards Obj 
+		_playerPos.GetComponent<Rigidbody>().AddForce(_direction.normalized * _force);
+	}
+}
---
+++ GravityProject/Assets/Scripts/Player/GravityController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour
+{
+
+	public bool IsGravityFlipped = false;
+	
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.E) && IsGravityFlipped == false)
+		{
+			IsGravityFlipped = true;
+		}
+		else
+		{
+			IsGravityFlipped = false;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		if (IsGravityFlipped == true)
+		{
+			Physics.gravity *= -1;
+			
+		}
+	}
+}
