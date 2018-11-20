cf330bf420dd1d566d26cea84c31bc8d31fe071c
Kacper Mazur <33032053+kacpermazur@users.noreply.github.com>
2018-11-20 20:33:06 +0000

    camera data

---
+++ GravityProject/Assets/Scripts/Camera/Data/CameraData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Camera.Data
+{
+	[CreateAssetMenuAttribute(fileName = "CameraData_")]
+	public class CameraData : ScriptableObject
+	{
+		public float FollowSpeed;
+		public float Smoothing;
+	}
+}
