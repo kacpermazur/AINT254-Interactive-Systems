8e562c24be4646347384a59165971e1d8dd3afe5
Kacper Mazur <33032053+kacpermazur@users.noreply.github.com>
2018-11-20 20:21:31 +0000

    CameraManger
    setting up the camera manger for better/easier camera controll

---
+++ GravityProject/Assets/Scripts/Camera/CameraManger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Camera
+{
+    public class CameraManger : MonoBehaviour, IInitializable
+    {
+        private static readonly string CameramMangerName = typeof(CameraManger).Name;
+        private static CameraManger _instance;
+
+        private Transform _cameraTarget;
+        private Transform _pivot;
+        private Transform _cameraTransform;
+
+        void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this;
+            }
+            
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            
+        }
+
+        private static void LogMessage(string message)
+        {
+            Debug.Log("<color=blue>" + CameramMangerName + "</color> : " + message);
+        }
+    }
+}
