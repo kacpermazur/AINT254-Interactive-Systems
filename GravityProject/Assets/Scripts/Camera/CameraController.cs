903182bcbef444c5ae29aa65a462254e9b959da4
Kacper Mazur <33032053+kacpermazur@users.noreply.github.com>
2018-11-20 22:40:22 +0000

    followTarget

---
+++ GravityProject/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Camera;
+using Camera.Data;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour, IInitializable
+{
+	private static readonly string CameraControllerName = typeof(CameraController).Name;
+
+	private CameraData _cameraData;
+	
+	private bool isInitialized;
+	
+	public void Initialize()
+	{
+		if (!isInitialized)
+		{
+			_cameraData = CameraManger.CameraDataConfig;
+			isInitialized = true;
+		}
+	}
+
+	void Update()
+	{
+		if (isInitialized)
+		{
+			FollowTarget();
+		}
+	}
+
+	private void FollowTarget()
+	{
+		Vector3 moveToTargetPos = Vector3.Lerp(transform.position, CameraManger.CameraTarget.position, _cameraData.FollowSpeed * Time.deltaTime);
+		transform.position = moveToTargetPos;
+	}
+
+	private static void LogMessage(string message)
+	{
+		Debug.Log("<color=blue>" + CameraControllerName + "</color> : " + message);
+	}
+}
--- GravityProject/Assets/Scripts/Camera/CameraManger.cs
+++ GravityProject/Assets/Scripts/Camera/CameraManger.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 
 namespace Camera
 {
+    [RequireComponent(typeof(CameraController))]
     public class CameraManger : MonoBehaviour, IInitializable
     {
         private static readonly string CameraMangerName = typeof(CameraManger).Name;
         private static CameraManger _instance;
 
-        private Transform _cameraTarget;
+        [SerializeField] private Transform _cameraTarget;
+        
         private Transform _pivot;
         private Transform _cameraTransform;
         
         [SerializeField] private CameraData _cameraDataConfig;
+
+        private CameraController _cameraController;
 
         public static CameraData CameraDataConfig
         {
@@ -31,6 +35,10 @@ namespace Camera
                 }
             }
         }
+        
+        public static Transform CameraTarget{ get { return _instance._cameraTarget; }}
+        public static Transform CameraPivot{ get { return _instance._pivot; }}
+        public static Transform CameraTransform{ get { return _instance._cameraTransform; }}
 
         void Awake()
         {
@@ -44,7 +52,8 @@ namespace Camera
 
         public void Initialize()
         {
-            
+            _cameraController = GetComponent<CameraController>();
+            _pivot = _cameraTarget.parent;
         }
 
         private static void LogMessage(string message)
