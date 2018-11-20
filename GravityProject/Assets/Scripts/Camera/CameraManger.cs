7b825a52b3502260cad4ca5da1c15a8a13621912
Kacper Mazur <33032053+kacpermazur@users.noreply.github.com>
2018-11-20 20:40:44 +0000

    linked data to the manger

--- GravityProject/Assets/Scripts/Camera/CameraManger.cs
+++ GravityProject/Assets/Scripts/Camera/CameraManger.cs
@@ -1,17 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Camera.Data;
 using UnityEngine;
 
 namespace Camera
 {
     public class CameraManger : MonoBehaviour, IInitializable
     {
-        private static readonly string CameramMangerName = typeof(CameraManger).Name;
+        private static readonly string CameraMangerName = typeof(CameraManger).Name;
         private static CameraManger _instance;
 
         private Transform _cameraTarget;
         private Transform _pivot;
         private Transform _cameraTransform;
+        
+        [SerializeField] private CameraData _cameraDataConfig;
+
+        public static CameraData CameraDataConfig
+        {
+            get
+            {
+                if (_instance._cameraDataConfig == null)
+                {
+                    LogMessage("PlayDataConfig Not Set Up");
+                    return null;
+                }
+                else
+                {
+                    return _instance._cameraDataConfig;
+                }
+            }
+        }
 
         void Awake()
         {
@@ -30,7 +49,7 @@ namespace Camera
 
         private static void LogMessage(string message)
         {
-            Debug.Log("<color=blue>" + CameramMangerName + "</color> : " + message);
+            Debug.Log("<color=blue>" + CameraMangerName + "</color> : " + message);
         }
     }
 }
