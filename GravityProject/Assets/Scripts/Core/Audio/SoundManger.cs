bf435a16850e5a5dd7b23204ae5bf402389cca18
Kacper Mazur <kacper.mazur888@gmail.com>
2018-12-11 20:00:33 +0000

    ...

--- GravityProject/Assets/Scripts/Core/Audio/SoundManger.cs
+++ GravityProject/Assets/Scripts/Core/Audio/SoundManger.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -52,10 +53,28 @@ namespace Core.Audio
 
 		public void PlaySound(string name, SoundType type)
 		{
-			switch (SoundType)
+			SoundClip selectedSound;
+			
+			switch (type)
 			{
 					case SoundType.SFX:
-						
+						selectedSound = Array.Find(_soundSfx, SoundClip => SoundClip.Name == name);
+						break;
+					case SoundType.MUSIC:
+						selectedSound = Array.Find(_soundMusic, SoundClip => SoundClip.Name == name);
+						break;
+					case SoundType.UI:
+						selectedSound = Array.Find(_soundUi, SoundClip => SoundClip.Name == name);
+						break;
+					default:
+						selectedSound = null;
+						LogMessage("Sound Not Found");
+						return;
+			}
+
+			if (selectedSound == null || selectedSound.Name != name)
+			{
+				LogMessage("Sound Not Found: Double Check Name");
 			}
 		}
 
