c4c0b2113fc6279f140636c99fcdcc1ffd2ea7e0
Kacper Mazur <kacper.mazur888@gmail.com>
2018-12-11 19:49:36 +0000

    audioMangerTypes

--- GravityProject/Assets/Scripts/Core/Audio/SoundManger.cs
+++ GravityProject/Assets/Scripts/Core/Audio/SoundManger.cs
@@ -7,20 +7,55 @@ namespace Core.Audio
 	public class SoundManger : MonoBehaviour, IInitializable
 	{
 		private static readonly string SoundMangerName = typeof(SoundManger).Name;
-		private static  SoundManger _instance;
+		private static SoundManger _instance;
+
+		private AudioSource _audioSourceMusic;
+		private AudioSource _audioSourceSfx;
 		
-		
+		//testing
+		public enum GameMode
+		{
+			STARTMENU,
+			INGAME,
+			PAUSE,
+		}
+		public enum SoundType
+		{
+			SFX,
+			MUSIC,
+			UI,
+		}
+
+		[SerializeField] private SoundClip[] _soundSfx;
+		[SerializeField] private SoundClip[] _soundMusic;
+		[SerializeField] private SoundClip[] _soundUi;
 		
 		private void Awake()
 		{
 			Initialize();
 		}
-		
+
+		private void Update()
+		{
+			
+		}
+
 		public void Initialize()
 		{
 			if (_instance == null)
 			{
 				_instance = this;
+			}
+
+			DontDestroyOnLoad(this);
+		}
+
+		public void PlaySound(string name, SoundType type)
+		{
+			switch (SoundType)
+			{
+					case SoundType.SFX:
+						
 			}
 		}
 
---
+++ GravityProject/Assets/Scripts/Core/Audio/SoundPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+	private SoundPlayer Instance;
+
+	public AudioSource AudioSourceMusic;
+	public AudioSource AudioSourceSfx;
+	public AudioClip currentClip;
+	
+	
+
+}
