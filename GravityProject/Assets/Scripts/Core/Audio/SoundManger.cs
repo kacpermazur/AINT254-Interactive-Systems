8697e0707eaf4e16509ac80d9c44c3e30d7ed81b
Kacper Mazur <kacper.mazur888@gmail.com>
2018-12-11 20:21:34 +0000

    audio Type Play

--- GravityProject/Assets/Scripts/Core/Audio/SoundManger.cs
+++ GravityProject/Assets/Scripts/Core/Audio/SoundManger.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
 using UnityEngine;
 
 namespace Core.Audio
@@ -48,6 +49,9 @@ namespace Core.Audio
 				_instance = this;
 			}
 
+			_audioSourceMusic = gameObject.AddComponent<AudioSource>();
+			_audioSourceSfx = gameObject.AddComponent<AudioSource>();
+
 			DontDestroyOnLoad(this);
 		}
 
@@ -59,25 +63,26 @@ namespace Core.Audio
 			{
 					case SoundType.SFX:
 						selectedSound = Array.Find(_soundSfx, SoundClip => SoundClip.Name == name);
+						_audioSourceSfx.clip = selectedSound.Audio;
+						_audioSourceSfx.Play();
 						break;
 					case SoundType.MUSIC:
 						selectedSound = Array.Find(_soundMusic, SoundClip => SoundClip.Name == name);
+						_audioSourceMusic.clip = selectedSound.Audio;
+						_audioSourceMusic.Play();
 						break;
 					case SoundType.UI:
 						selectedSound = Array.Find(_soundUi, SoundClip => SoundClip.Name == name);
+						_audioSourceSfx.clip = selectedSound.Audio;
+						_audioSourceSfx.Play();
 						break;
 					default:
 						selectedSound = null;
 						LogMessage("Sound Not Found");
 						return;
-			}
-
-			if (selectedSound == null || selectedSound.Name != name)
-			{
-				LogMessage("Sound Not Found: Double Check Name");
-			}
+			}	
 		}
-
+		
 		private static void LogMessage(string message)
 		{
 			Debug.Log("<color=blue>" + SoundMangerName + "</color> : " + message);
