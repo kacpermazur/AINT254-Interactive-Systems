55abb37aa21ea78c6dd22f153059b4740be5d075
Kacper Mazur <kacper.mazur888@gmail.com>
2018-12-12 06:39:09 +0000

    gameManger v0.1a
    very very beginning of a manger script

--- GravityProject/Assets/Scripts/Core/GameManger.cs
+++ GravityProject/Assets/Scripts/Core/GameManger.cs
@@ -8,6 +8,9 @@ namespace Core
     {
         private static readonly string GameMangerObjectName = typeof(GameManger).Name;
         private static GameManger _instance;
+
+        private static bool _hasPlayerStarted;
+        private static bool _hasPlayerDied;
         
         public enum GameState
         {
@@ -16,10 +19,13 @@ namespace Core
             PAUSE
         }
 
-        private GameState _currentGameState;
+        private static GameState _currentGameState;
         
-        public void UpdateGameState(GameState type){ _currentGameState = type; }
-        public GameState GetCurrentGameState() { return _currentGameState; }
+        public static void UpdateGameState(GameState type){ _currentGameState = type; }
+        public static GameState GetCurrentGameState() { return _currentGameState; }
+
+        public static bool SetPlayerStarted { set {  _hasPlayerStarted = value; } }
+        public static bool SetPlayerDied { set {  _hasPlayerDied = value; } }
 
         void Awake()
         {
@@ -33,13 +39,50 @@ namespace Core
             DontDestroyOnLoad(this);
         }
 
+        private void Update()
+        {
+            switch (_currentGameState)
+            {
+                    case GameState.MENU:
+                        //
+                        break;
+                    case GameState.GAME:
+                        onGameState();
+                        break;
+                    case GameState.PAUSE:
+                        //
+                        break;
+            }
+        }
+
         public void Initialize()
         {
             // Testing
             _currentGameState = GameState.GAME;
         }
-        
-        
+
+        private void onGameState()
+        {
+            float timer = 0f;
+            int deaths = 0;
+
+            if (_hasPlayerStarted)
+            {
+                timer += Time.time;
+
+                if (_hasPlayerDied)
+                {
+                    deaths += 1;
+                }
+            }
+            else
+            {
+                timer = 0f;
+            }
+            
+            
+
+        }
 
     }
 }
