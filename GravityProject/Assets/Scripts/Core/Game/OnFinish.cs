89298ab46fc867a36e7ec61ad09d4417ba44698e
Kacper Mazur <kacper.mazur888@gmail.com>
2018-12-12 07:31:53 +0000

    ...

---
+++ GravityProject/Assets/Scripts/Core/Game/OnFinish.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Game
+{
+	public class OnFinish : MonoBehaviour
+	{
+		private void OnTriggerEnter(Collider other)
+		{
+			if (other.gameObject.CompareTag("Player"))
+			{
+				GameManger.SetPlayerFinished = true;
+			}
+
+		}
+	}
+}
