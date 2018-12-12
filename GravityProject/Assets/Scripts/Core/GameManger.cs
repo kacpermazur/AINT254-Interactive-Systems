3dc13d3882c83aca71732d2cefdcf71df9939e9e
Kacper Mazur <kacper.mazur888@gmail.com>
2018-12-12 06:18:38 +0000

    ...

--- GravityProject/Assets/Scripts/Core/GameManger.cs
+++ GravityProject/Assets/Scripts/Core/GameManger.cs
@@ -4,10 +4,42 @@ using UnityEngine;
 
 namespace Core
 {
-    public class GameManger : MonoBehaviour
+    public class GameManger : MonoBehaviour, IInitializable
     {
         private static readonly string GameMangerObjectName = typeof(GameManger).Name;
         private static GameManger _instance;
+        
+        public enum GameState
+        {
+            MENU,
+            GAME,
+            PAUSE
+        }
+
+        private GameState _currentGameState;
+        
+        public void UpdateGameState(GameState type){ _currentGameState = type; }
+        public GameState GetCurrentGameState() { return _currentGameState; }
+
+        void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this;
+            }
+            
+            Initialize();
+            
+            DontDestroyOnLoad(this);
+        }
+
+        public void Initialize()
+        {
+            // Testing
+            _currentGameState = GameState.GAME;
+        }
+        
+        
 
     }
 }
