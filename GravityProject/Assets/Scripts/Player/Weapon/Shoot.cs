e4ca61410de7076143e54168d74e9281c3c027a7
Kacper Mazur <kacper.mazur888@gmail.com>
2019-01-03 22:39:33 +0000

    Code refactoring

---
+++ GravityProject/Assets/Scripts/Player/Weapon/Shoot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.Input;
+
+namespace Player.Weapon
+{
+	public class Shoot : MonoBehaviour, IInitializable
+	{
+		private Bullet _shootingBullet;
+		
+		public void Initialize()
+		{
+			_shootingBullet = GetComponent<Bullet>();
+		}
+	
+	}
+}
